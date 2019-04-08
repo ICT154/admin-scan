c1a54465f3fbf17ab240c420302c26a1d4b7d767
widhisec <39062772+widhisec@users.noreply.github.com>
2019-04-09 00:50:53 +0700

    Add files via upload

--- admin.sh
+++ admin.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# last update : 08/04/2019
 clear;clear
 GREEN=$(tput setaf 2) #HIJAU
 WHITE=$(tput setaf 7) #PUTIH
@@ -6,6 +7,12 @@ CYAN=$(tput setaf 6) #CYAN
 RED=$(tput setaf 1) #MERAH
 NORMAL=$(tput sgr0)
 function check(){
+wget -q --spider http://google.com
+if [ $? -eq 0 ]; then
+    echo "online ..."
+else
+    echo "hidupkan koneksi internet.."
+fi
 if [[ ! -z $(command -v lynx) ]]; then
 	printf ""
 else
@@ -15,7 +22,6 @@ else
 fi
 return
 }
-check
 function banner(){
 printf "${RED}
 \t[ c0der by widhisec       ]
@@ -25,9 +31,9 @@ printf "${RED}
 }
 function scanlgsg(){
 fc=$(lynx -head -dump "$urls/$x" | grep -n "1" | cut -d "8" -f1 | cut -d "6" -f3 | cut -d "9" -f6 | cut -d "5" -f1 | tr -d "4" | grep --text "1:.*" | awk '/:/{print $2}')
-stas; return
+status; return
 }
-stas(){
+status(){
 printf "${GREEN}STATUS : ${WHITE}%s$fc => $urls/$x \n"
 }
 printf ""
